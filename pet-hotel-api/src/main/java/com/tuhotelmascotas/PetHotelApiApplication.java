e19ca28f03d74adf778450033066422d57a3d244
Ing-Mapache-coder <jesusdanielle@ufps.edu.co>
2025-05-09 10:46:41 -0500

    Primer commit.

---
+++ pet-hotel-api/src/main/java/com/tuhotelmascotas/PetHotelApiApplication.java
@@ -0,0 +1,15 @@
+package com.tuhotelmascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHotelApiApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PetHotelApiApplication.class, args);
+		
+	}
+
+}
